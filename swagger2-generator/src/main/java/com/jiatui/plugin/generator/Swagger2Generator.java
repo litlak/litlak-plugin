fdf057482a72f46f60745bb2f32b71e886567c6a
scomy <queshenhai@163.com>
2019-05-09 14:11:04 +0800

    swagger2 gernerator develop

---
+++ swagger2-generator/src/main/java/com/jiatui/plugin/generator/Swagger2Generator.java
@@ -0,0 +1,80 @@
+package com.jiatui.plugin.generator;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+/**
+ * 
+ * @author Scomy
+ *
+ * @date 2019-03-28
+ */
+public class Swagger2Generator extends PluginAdapter {
+
+	@Override
+	public boolean validate(List<String> warnings) {
+		return true;
+	}
+
+	@Override
+	public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+			IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+
+		if (!contains(topLevelClass.getAnnotations(), "@ApiModel")) {
+			boolean shortModelValue = Boolean.getBoolean(properties.getProperty("shortModelValue"));
+			FullyQualifiedJavaType type = topLevelClass.getType();
+			String modelValue = shortModelValue ? type.getShortName() : type.toString();
+			String remarks = introspectedTable.getRemarks();
+			String classAnnotation = "@ApiModel(value = \"" + modelValue + "\", description = \"" + remarks + "\")";
+			topLevelClass.addAnnotation(classAnnotation);
+		}
+
+		Set<FullyQualifiedJavaType> importedTypes = topLevelClass.getImportedTypes();
+		FullyQualifiedJavaType modelJavaType = new FullyQualifiedJavaType("io.swagger.annotations.ApiModel");
+		if (!importedTypes.contains(modelJavaType)) {
+			topLevelClass.addImportedType(modelJavaType);
+		}
+		FullyQualifiedJavaType propertyJavaType = new FullyQualifiedJavaType("io.swagger.annotations.ApiModelProperty");
+		if (!importedTypes.contains(propertyJavaType)) {
+			topLevelClass.addImportedType(propertyJavaType);
+		}
+
+		List<String> fieldAnnotations = field.getAnnotations();
+		if (!contains(fieldAnnotations, "@ApiModelProperty")) {
+			field.addAnnotation(generatorAnnotation(introspectedColumn));
+		}
+		return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+	}
+
+	private String generatorAnnotation(IntrospectedColumn introspectedColumn) {
+		StringBuilder sb = new StringBuilder("@ApiModelProperty(");
+
+		sb.append("value = \"" + introspectedColumn.getRemarks() + "\"");// 名字
+		sb.append(", ");
+		sb.append("required = " + !introspectedColumn.isNullable() + "");// 是否必选
+		if (introspectedColumn.getDefaultValue() != null) {
+			sb.append(", ");
+			sb.append("example = \"" + introspectedColumn.getDefaultValue() + "\"");// 默认值
+		}
+
+		sb.append(')');
+		return sb.toString();
+	}
+
+	private boolean contains(Collection<String> list, String string0) {
+		for (String string : list) {
+			if (string.contains(string0)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
